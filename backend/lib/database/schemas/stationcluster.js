f27c7251fe392876787e1900fcb219d272ee604d
ReneHollander <rene.hollander@hotmail.de>
2015-11-04 21:52:09 +0100

    added nextStation to stationcluster (wip)

--- backend/lib/database/schemas/stationcluster.js
+++ backend/lib/database/schemas/stationcluster.js
@@ -34,6 +34,19 @@ module.exports = function (schemas) {
         return this.find({_id: id}).deepPopulate(['stations.navigation', 'stations.answer']);
     };
 
+    schema.statics.nextStation = function (current, completedClusters) {
+        return this.find({stations: current._id}).deepPopulate(['stations.navigation', 'stations.answer'])
+            .then(function (cluster) {
+                return cluster.nextStation(current);
+            })
+            .then(function (station) {
+                if (station) return station;
+                else {
+                    // todo find random station that was not already completed
+                }
+            });
+    };
+
     schema.methods.getStations = function () {
         return this.stations;
     };
@@ -45,6 +58,17 @@ module.exports = function (schemas) {
     schema.methods.addStation = function (station) {
         this.stations.push(station);
         return this.save().then(util.promiseDeepPopulate(['stations.navigation', 'stations.answer']));
+    };
+
+    schema.methods.nextStation = function (station) {
+        if (station) {
+            var currindex = this.stations.indexOf(station);
+            if (currindex == -1) return Promise.reject(new Error('Station not in cluster'));
+            if (this.stations.length - 1 == currindex) return Promise.resolve(undefined);
+            else return this.stations[currindex + 1].populate(['navigation', 'answer']);
+        } else {
+            return this.stations[0].populate(['navigation', 'answer']);
+        }
     };
 
     return mongoose.model('StationCluster', schema);
